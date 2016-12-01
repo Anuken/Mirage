87ae05770f5e779488ac55182b9271be5e25bea8
Anuken <arnukren@gmail.com>
2016-11-30 23:32:55 -0500

    Final minor changes (?)

--- desktop/src/net/pixelstatic/Mirage/desktop/DesktopLauncher.java
+++ desktop/src/net/pixelstatic/Mirage/desktop/DesktopLauncher.java
@@ -1,8 +1,5 @@
 package net.pixelstatic.Mirage.desktop;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
-
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
@@ -12,8 +9,9 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setDecorated(false);
-		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-		config.setWindowedMode(d.width, d.height);
+		//Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+		//config.setWindowedMode(d.width, d.height);
+		config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
 		config.setTitle("Mirage");
 		new Lwjgl3Application(new Mirage(), config);
 	}
